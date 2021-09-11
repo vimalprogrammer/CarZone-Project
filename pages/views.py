aa4ca307e6fb44a4ca6cd6e868a965163f6d1208
Vimal M <msvimal10@gmail.com>
2021-09-11 13:42:15 +0530

    html created inside templates folder

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html') # home.html not yet created in templates
+    return render(request, 'pages/home.html')
