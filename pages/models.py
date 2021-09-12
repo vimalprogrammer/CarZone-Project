278393704e30dd8c59f6595ff0d6b02e9ce77cd5
Vimal M <msvimal10@gmail.com>
2021-09-12 14:09:31 +0530

    Model Teams for created for backend details

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Teams(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    designation = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')#pillow library installed
+    facebook = models.URLField(max_length=100)
+    twitter = models.URLField(max_length=100)
+    google_plus = models.URLField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
