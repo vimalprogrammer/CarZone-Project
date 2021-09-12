1ed1f2751cb295c8090aa0341a45d972e3e444aa
Vimal M <msvimal10@gmail.com>
2021-09-12 14:47:09 +0530

    Image workin process in admin page

--- CarZone/settings.py
+++ CarZone/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'CarZone/static'),
 ]
+
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL =  '/media/'
